a7e5cf98687754f2106d9987e15172e9d5bc75cb
Georg Jaedicke <georg.jaedicke@gmail.com>
2016-03-19 18:11:57 +0100

    modules.php löschen

--- tsv-parallax/languages/de/modules.php
+++
@@ -1,7 +0,0 @@
-<?php
-$GLOBALS['TL_LANG']['FMD']['parallax'][0] = 'Parallax';
-
-$GLOBALS['TL_LANG']['FMD']['parallaxStart'][0] = 'Parallax Start';
-$GLOBALS['TL_LANG']['FMD']['parallaxStart'][1] = 'Hier beginnt der Parallax-Inhalt';
-$GLOBALS['TL_LANG']['FMD']['parallaxEnd'][0] = 'Parallax Ende';
-$GLOBALS['TL_LANG']['FMD']['parallaxEnd'][1] = 'Hier endet der Parallax-Inhalt';
